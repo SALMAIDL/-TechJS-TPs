b517939cbf10bc56558301a4eb5437589dff1b47
achrafzahid <salmalamghari44@gmail.com>
2026-06-07 21:14:39 +0100

    tp2, pokemon-cli, book-reading-tracker

--- src/middleware/auth.js
+++
@@ -1,7 +0,0 @@
-function isAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) return next();
-  req.session.returnTo = req.originalUrl;
-  res.redirect("/auth/login");
-}
-
-module.exports = { isAuthenticated };
